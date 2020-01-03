955807ea961c978fef37d9dbc962a1eb35e36811
liuyang <3l>
2020-01-03 12:51:21 +0800

    UpData:Add functionality support UDIM Textures

--- Core/Publish_Method.py
+++ Core/Publish_Method.py
@@ -3,20 +3,23 @@
 # @Author   :Fighter
 # @Software :Heater
 # @E-mail    :317431629@qq.com
-from Core import Publish_UI
-reload(Publish_UI)
 from PySide2 import QtWidgets
 from PySide2 import QtGui
 import shutil
 import os
 import pymel.core as pm
 import sys
+import json
+from Core import Publish_UI
+reload(Publish_UI)
 
 
 class PublishFoo(Publish_UI.PublishUI):
 	def __init__(self):
 		super(PublishFoo, self).__init__()
-		self.image_type = ['file', 'RedshiftSprite', 'RedshiftNormalMap']
+		self.data_path = os.path.join(sys.path[-1], 'Data', "Shader_type.json")
+		with open(self.data_path, "r") as f:
+			self.image_type = json.loads(f.read())
 		self.library_path = "{}/Library".format(sys.path[-1])
 		self.connect_ui()
 
@@ -34,8 +37,14 @@ class PublishFoo(Publish_UI.PublishUI):
 	def get_assets_data(self):
 		assets_name = self.assets_name_line.text()
 		assets_type = self.assets_type_line.text()
-		assets_path = os.path.join(self.library_path, assets_type, assets_name, "{}.ma".format(assets_name)).replace('\\', '/')
-		assets_image = os.path.join(self.library_path, assets_type, assets_name, "{}.png".format(assets_name)).replace('\\', '/')
+		assets_path = os.path.join(
+									self.library_path, assets_type, assets_name,
+									"{}.ma".format(assets_name)
+									).replace('\\', '/')
+		assets_image = os.path.join(
+									self.library_path, assets_type, assets_name,
+									"{}.png".format(assets_name)
+									).replace('\\', '/')
 		return assets_name, assets_type, assets_path, assets_image
 
 	@staticmethod
@@ -47,43 +56,39 @@ class PublishFoo(Publish_UI.PublishUI):
 		pm.saveAs(path)
 		print(u'文件另存为：{}'.format(path))
 
-	def get_maya_image(self, image_type):  # todo: 优化下这个函数
+	def get_maya_image(self):
 		assets_name, assets_type, assets_path, assets_image = self.get_assets_data()
-		if image_type == 'file':
-			image_nodes = pm.ls(type=image_type)
-			for image_node in image_nodes:
-				image_path = image_node.fileTextureName.get()
-				image_name = os.path.split(image_path)[-1]
-				source_image_path = os.path.join(self.library_path, assets_type, assets_name, 'Sourceimages')
-				new_image_name = os.path.join(source_image_path, image_name)
-				self.copy_source_image(image_path, new_image_name)
-				image_node.fileTextureName.set(new_image_name)
-		else:
-			image_nodes = pm.ls(type=image_type)
-			for image_node in image_nodes:
-				image_path = image_node.tex0.get()
-				image_name = os.path.split(image_path)[-1]
-				source_image_path = os.path.join(self.library_path, assets_type, assets_name, 'Sourceimages')
-				new_image_name = os.path.join(source_image_path, image_name)
-				self.copy_source_image(image_path, new_image_name)
-				image_node.tex0.set(new_image_name)
+		texture = pm.ls(textures=True)
+		for i in texture:
+			image_attr = pm.PyNode("{}.{}".format(i, self.image_type[str(type(i))]))
+			image_path, image_name = os.path.split(image_attr.get())
+			source_image_path = os.path.join(self.library_path, assets_type, assets_name, 'Sourceimages')
+			image_attr.set(os.path.join(source_image_path, image_name))
+			self.copy_source_image(image_name, image_path, source_image_path)
 
 	@staticmethod
-	def copy_source_image(image_path, new_image_path):
-		source_image_path = os.path.split(new_image_path)[0]
+	def copy_source_image(image_name, image_path, source_image_path):
 		if os.path.exists(source_image_path):
 			pass
 		else:
 			os.makedirs(source_image_path)
-		shutil.copy2(image_path, new_image_path)
+		udim_images = [
+						udim_name for udim_name in os.listdir(image_path)
+						if image_name.split("<UDIM>")[0] in udim_name and "<UDIM>" in image_name
+					]
+		if udim_images:
+			for i in udim_images:
+				shutil.copy2(os.path.join(image_path, i), os.path.join(source_image_path, i))
+		else:
+			shutil.copy2(os.path.join(image_path, image_name), os.path.join(source_image_path, image_name))
+
 	# _________import Mod_______________________#
 
 	def publish_to_library(self):
 		assets_name, assets_type, assets_path, assets_image = self.get_assets_data()
 		# # ________________copy to library____________________________________________#
 
-		for image_type in self.image_type:
-			self.get_maya_image(image_type)  # 拷贝贴图文件
+		self.get_maya_image()  # 拷贝贴图文件
 
 		self.save_file(assets_path)  # 文件另存为
 
